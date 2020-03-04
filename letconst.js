c9a88da04dfb73569bc31856c6e0ee996fc3866a
mazharultech <mazharulislam807064@gmail.com>
2020-03-04 23:37:47 +0600

    es6 updated

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const hubby = "saniya";
+// hubby = "taniya"
+// console.log(hubby);
+const numbers = [12, 34];
+numbers[1] = 509;
+numbers.push(120);
+// console.log(numbers);
+
+// const name = { name: "shakib", phone: 3455 };
+
+// console.log(name);
+
+
+let userName = "tareq rahman";
+userName = "shakib Khan"
+console.log(userName);
+
+let sum = 0;
+for (let i = 1; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(i);
---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Mazharul";
+const lastName = "Islam";
+const fullName = firstName + " " + lastName + " I love you janu";
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+const multiline = "I love you\n" +
+    "i like you\n" +
+    "I miss you\n" +
+    "I need you";
+const multiline2 = `I love you 
+I need you
+I miss you
+I like you
+I don't need you
+I don't like you`;
+console.log(multiline2);
